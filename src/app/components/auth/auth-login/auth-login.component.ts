6903ccc49c41ef5ad62617238fdb819e842c6bfc
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2024-11-16 15:56:28 -0500

    Estilos completados del login

--- src/app/components/auth/auth-login/auth-login.component.ts
+++ src/app/components/auth/auth-login/auth-login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-auth-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth-login.component.html',
   styleUrl: './auth-login.component.scss'
 })
 export class AuthLoginComponent {
+
+  formulario!: FormGroup
+  form = inject(FormBuilder)
+
+  ngOnInit(): void {
+
+    this.formulario = this.form.group({
+      email: this.form.control('', [Validators.required, Validators.email]),
+      password: this.form.control('', [Validators.required])
+    })
+
+  }
 
 }
