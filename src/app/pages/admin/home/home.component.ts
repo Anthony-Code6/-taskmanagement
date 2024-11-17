6050008be1ba311e53c1fc5731b3173f521cbd81
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2024-11-17 09:14:35 -0500

    link de agregar y shadon en los card

--- src/app/pages/admin/home/home.component.ts
+++ src/app/pages/admin/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeWorkComponent } from "../../../components/admin/home/home-work/home-work.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeWorkComponent],
+  imports: [HomeWorkComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
